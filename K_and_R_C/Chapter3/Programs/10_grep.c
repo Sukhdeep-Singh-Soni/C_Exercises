c7ac98ccc1294bcb4c4052601c9fd81527dce6a0
Sukhdeep-Singh-Soni <sukhdeepsinghsoni1998@gmail.com>
2025-08-14 18:01:47 +0530

    added to k and r c

--- K_and_R_C/Chapter3/Programs/10_grep.c
+++ K_and_R_C/Chapter3/Programs/10_grep.c
@@ -16,7 +16,16 @@ int str_index(char s[], char t[]);
 char pattern[] = "ould";    /* pattern to search for */
 
 main() {
+    char line[MAXLINE];
+    int found;
 
+    found = 0;
+    while (get_line(line, MAXLINE) > 0)
+        if (str_index(line, pattern) >= 0) {
+            printf("%s", line);
+            found++;
+        }
+    return found;
 }
 
 /* get_line: get lines into s, return length */
@@ -25,7 +34,7 @@ int get_line(char s[], int lim) {
 
     i = 0;
     while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-        s[i] = c;
+        s[i++] = c;
     if (c == '\n')
         s[i++] = c;
     s[i] = '\0';
@@ -34,5 +43,13 @@ int get_line(char s[], int lim) {
 
 /* str_index: return index to t in s, -1 if none */
 int str_index(char s[], char t[]) {
-    
+    int i, j, k;
+
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i, k = 0; t[k] != '\0' && s[j]==t[k]; k++, j++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
 }
